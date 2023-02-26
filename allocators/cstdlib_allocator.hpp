d6dbe1adcb5cbff8edeb2cbf234f72e845f572b1
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-02-26 12:42:30 +0200

    cstdlib_allocator: Remove leak test
    This can be done through other means or added back in really easily.
    Having it makes failing tests crash.

--- allocators/cstdlib_allocator.hpp
+++ allocators/cstdlib_allocator.hpp
@@ -13,30 +13,13 @@ class CstdlibAllocator : public Allocator
 {
   public:
     CstdlibAllocator() { }
-    virtual ~CstdlibAllocator()
-    {
-        assert(m_alloc_count == 0 && "CstdlibAllocator allocations leaked");
-        m_alloc_count = 0;
-    }
 
     virtual void *allocate(size_t num_bytes) override
     {
-        m_alloc_count++;
         return std::malloc(num_bytes);
     }
 
-    virtual void deallocate(void *ptr)
-    {
-        if (ptr != nullptr)
-        {
-            std::free(ptr);
-            assert(m_alloc_count > 0);
-            m_alloc_count--;
-        }
-    }
-
-  private:
-    size_t m_alloc_count{0};
+    virtual void deallocate(void *ptr) override { std::free(ptr); }
 };
 
 } // namespace wheels
