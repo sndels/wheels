c2083a7db40c496abc0c9387bfa62cddd9ca7adc
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-02-26 16:26:42 +0200

    Add SmalMap

---
+++ containers/small_map.hpp
@@ -0,0 +1,196 @@
+
+#ifndef WHEELS_SMALL_MAP_HPP
+#define WHEELS_SMALL_MAP_HPP
+
+#include "container_utils.hpp"
+#include "pair.hpp"
+#include "static_array.hpp"
+
+namespace wheels
+{
+
+template <typename K, typename V, size_t N> class SmallMap
+{
+  public:
+    SmallMap(){};
+    ~SmallMap();
+
+    SmallMap(SmallMap<K, V, N> const &other);
+    SmallMap(SmallMap<K, V, N> &&other);
+    SmallMap<K, V, N> &operator=(SmallMap<K, V, N> const &other);
+    SmallMap<K, V, N> &operator=(SmallMap<K, V, N> &&other);
+
+    Pair<K, V> *begin();
+    Pair<K, V> const *begin() const;
+    Pair<K, V> *end();
+    Pair<K, V> const *end() const;
+
+    bool empty() const;
+    size_t size() const;
+    size_t capacity() const;
+
+    bool contains(K const &key) const;
+    V *find(K const &key);
+    V const *find(K const &key) const;
+
+    void clear();
+    void insert_or_assign(K &&key, V &&value);
+    void insert_or_assign(Pair<K, V> &&key_value);
+    void remove(K const &key);
+
+  private:
+    StaticArray<Pair<K, V>, N> m_data;
+};
+
+template <typename K, typename V, size_t N> SmallMap<K, V, N>::~SmallMap()
+{
+    clear();
+}
+
+template <typename K, typename V, size_t N>
+SmallMap<K, V, N>::SmallMap(SmallMap<K, V, N> const &other)
+: m_data{other.m_data}
+{
+}
+
+template <typename K, typename V, size_t N>
+SmallMap<K, V, N>::SmallMap(SmallMap<K, V, N> &&other)
+: m_data{WHEELS_MOV(other.m_data)}
+{
+}
+
+template <typename K, typename V, size_t N>
+SmallMap<K, V, N> &SmallMap<K, V, N>::operator=(SmallMap<K, V, N> const &other)
+{
+    if (this != &other)
+        m_data = other.m_data;
+
+    return *this;
+}
+
+template <typename K, typename V, size_t N>
+SmallMap<K, V, N> &SmallMap<K, V, N>::operator=(SmallMap<K, V, N> &&other)
+{
+    if (this != &other)
+        m_data = WHEELS_MOV(other.m_data);
+
+    return *this;
+}
+
+template <typename K, typename V, size_t N>
+Pair<K, V> *SmallMap<K, V, N>::begin()
+{
+    return m_data.begin();
+}
+
+template <typename K, typename V, size_t N>
+Pair<K, V> const *SmallMap<K, V, N>::begin() const
+{
+    return m_data.begin();
+}
+
+template <typename K, typename V, size_t N> Pair<K, V> *SmallMap<K, V, N>::end()
+{
+    return m_data.end();
+}
+
+template <typename K, typename V, size_t N>
+Pair<K, V> const *SmallMap<K, V, N>::end() const
+{
+    return m_data.end();
+}
+
+template <typename K, typename V, size_t N>
+bool SmallMap<K, V, N>::empty() const
+{
+    return m_data.empty();
+}
+
+template <typename K, typename V, size_t N>
+size_t SmallMap<K, V, N>::size() const
+{
+    return m_data.size();
+}
+
+template <typename K, typename V, size_t N>
+size_t SmallMap<K, V, N>::capacity() const
+{
+    return m_data.capacity();
+}
+
+template <typename K, typename V, size_t N>
+bool SmallMap<K, V, N>::contains(K const &key) const
+{
+    for (auto const &kv : m_data)
+    {
+        if (kv.first == key)
+            return true;
+    }
+
+    return false;
+}
+
+template <typename K, typename V, size_t N>
+V *SmallMap<K, V, N>::find(K const &key)
+{
+    for (auto &kv : m_data)
+    {
+        if (kv.first == key)
+            return &kv.second;
+    }
+
+    return nullptr;
+}
+
+template <typename K, typename V, size_t N>
+V const *SmallMap<K, V, N>::find(K const &key) const
+{
+    for (auto const &kv : m_data)
+    {
+        if (kv.first == key)
+            return &kv.second;
+    }
+
+    return nullptr;
+}
+
+template <typename K, typename V, size_t N> void SmallMap<K, V, N>::clear()
+{
+    m_data.clear();
+}
+
+template <typename K, typename V, size_t N>
+void SmallMap<K, V, N>::insert_or_assign(K &&key, V &&value)
+{
+    if (V *v = find(key); v != nullptr)
+        *v = value;
+    else
+        m_data.emplace_back(WHEELS_FWD(key), WHEELS_FWD(value));
+}
+
+template <typename K, typename V, size_t N>
+void SmallMap<K, V, N>::insert_or_assign(Pair<K, V> &&key_value)
+{
+    if (V *v = find(key_value.first); v != nullptr)
+        *v = key_value.second;
+    else
+        m_data.emplace_back(WHEELS_FWD(key_value));
+}
+
+template <typename K, typename V, size_t N>
+void SmallMap<K, V, N>::remove(K const &key)
+{
+    for (auto &v : m_data)
+    {
+        if (v.first == key)
+        {
+            std::swap(v, m_data.back());
+            m_data.pop_back();
+            return;
+        }
+    }
+}
+
+} // namespace wheels
+
+#endif // WHEELS_SMALL_MAP_HPP
--- containers/test.cpp
+++ containers/test.cpp
@@ -3,6 +3,7 @@
 #include "allocators/cstdlib_allocator.hpp"
 #include "array.hpp"
 #include "pair.hpp"
+#include "small_map.hpp"
 #include "small_set.hpp"
 #include "static_array.hpp"
 
@@ -192,6 +193,30 @@ SmallSet<DtorObj, N> init_test_small_set_dtor(size_t initial_size)
         set.insert(DtorObj{10 * (i + 1)});
 
     return set;
+}
+
+template <size_t N>
+SmallMap<uint32_t, uint32_t, N> init_test_small_map_u32(size_t initial_size)
+{
+    assert(initial_size <= N);
+
+    SmallMap<uint32_t, uint32_t, N> map;
+    for (uint32_t i = 0; i < initial_size; ++i)
+        map.insert_or_assign(10 * (i + 1), 10 * (i + 1) + 1);
+
+    return map;
+}
+
+template <size_t N>
+SmallMap<DtorObj, DtorObj, N> init_test_small_map_dtor(size_t initial_size)
+{
+    assert(initial_size <= N);
+
+    SmallMap<DtorObj, DtorObj, N> map;
+    for (uint32_t i = 0; i < initial_size; ++i)
+        map.insert_or_assign(make_pair(DtorObj{10 * i}, DtorObj{10 * (i + 1)}));
+
+    return map;
 }
 
 } // namespace
@@ -870,4 +895,151 @@ TEST_CASE("Pair", "[test]")
     REQUIRE(p0 == p1);
     REQUIRE(p0 != p2);
     REQUIRE(p0 != p3);
+}
+
+TEST_CASE("SmallMap::allocate_copy", "[test]")
+{
+    SmallMap<uint32_t, uint16_t, 4> map;
+    REQUIRE(map.empty());
+    REQUIRE(map.size() == 0);
+    REQUIRE(map.capacity() == 4);
+
+    map.insert_or_assign(10, 11);
+    map.insert_or_assign(20, 21);
+    map.insert_or_assign(30, 31);
+    REQUIRE(!map.empty());
+    REQUIRE(map.size() == 3);
+
+    REQUIRE(map.contains(10));
+    REQUIRE(map.contains(20));
+    REQUIRE(map.contains(30));
+    REQUIRE(!map.contains(40));
+    REQUIRE(*map.find(10) == 11);
+    REQUIRE(*map.find(20) == 21);
+    REQUIRE(*map.find(30) == 31);
+    REQUIRE(map.find(40) == nullptr);
+    map.insert_or_assign(make_pair((uint32_t)30, (uint16_t)29));
+    REQUIRE(map.size() == 3);
+    REQUIRE(*map.find(30) == 29);
+    map.insert_or_assign(make_pair((uint32_t)30, (uint16_t)31));
+
+    SmallMap<uint32_t, uint16_t, 4> map_copy_constructed{map};
+    REQUIRE(*map_copy_constructed.find(10) == 11);
+    REQUIRE(*map_copy_constructed.find(20) == 21);
+    REQUIRE(*map_copy_constructed.find(30) == 31);
+    REQUIRE(map_copy_constructed.size() == 3);
+
+    SmallMap<uint32_t, uint16_t, 4> map_copy_assigned;
+    map_copy_assigned = map_copy_constructed;
+    map_copy_assigned = map_copy_assigned;
+    REQUIRE(*map_copy_assigned.find(10) == 11);
+    REQUIRE(*map_copy_assigned.find(20) == 21);
+    REQUIRE(*map_copy_assigned.find(30) == 31);
+    REQUIRE(map_copy_assigned.size() == 3);
+
+    SmallMap<uint32_t, uint16_t, 4> map_move_constructed{WHEELS_MOV(map)};
+    REQUIRE(*map_move_constructed.find(10) == 11);
+    REQUIRE(*map_move_constructed.find(20) == 21);
+    REQUIRE(*map_move_constructed.find(30) == 31);
+    REQUIRE(map_move_constructed.size() == 3);
+
+    SmallMap<uint32_t, uint16_t, 4> map_move_assigned;
+    map_move_assigned = WHEELS_MOV(map_move_constructed);
+    map_move_assigned = WHEELS_MOV(map_move_assigned);
+    REQUIRE(*map_move_assigned.find(10) == 11);
+    REQUIRE(*map_move_assigned.find(20) == 21);
+    REQUIRE(*map_move_assigned.find(30) == 31);
+    REQUIRE(map_move_assigned.size() == 3);
+}
+
+TEST_CASE("SmallMap::begin_end", "[test]")
+{
+    SmallMap<uint32_t, uint32_t, 3> map = init_test_small_map_u32<3>(3);
+    REQUIRE(map.size() == 3);
+    REQUIRE(map.begin() == map.end() - 3);
+
+    SmallMap<uint32_t, uint32_t, 3> const &map_const = map;
+    REQUIRE(map_const.begin() == map_const.end() - 3);
+}
+
+TEST_CASE("SmallMap::clear", "[test]")
+{
+    init_dtor_counters();
+    SmallMap<DtorObj, DtorObj, 5> map = init_test_small_map_dtor<5>(5);
+    REQUIRE(DtorObj::s_ctor_counter == 30);
+    REQUIRE(DtorObj::s_value_ctor_counter == 10);
+    REQUIRE(DtorObj::s_copy_ctor_counter == 0);
+    REQUIRE(DtorObj::s_move_ctor_counter == 20);
+    REQUIRE(DtorObj::s_assign_counter == 0);
+    REQUIRE(DtorObj::s_dtor_counter == 0);
+    REQUIRE(!map.empty());
+    REQUIRE(map.size() == 5);
+    REQUIRE(map.capacity() == 5);
+
+    map.clear();
+    REQUIRE(map.empty());
+    REQUIRE(map.size() == 0);
+    REQUIRE(map.capacity() == 5);
+    REQUIRE(DtorObj::s_ctor_counter == 30);
+    REQUIRE(DtorObj::s_assign_counter == 0);
+    REQUIRE(DtorObj::s_dtor_counter == 10);
+}
+
+TEST_CASE("SmallMap::remove", "[test]")
+{
+    SmallMap<uint32_t, uint32_t, 3> map = init_test_small_map_u32<3>(3);
+    REQUIRE(map.size() == 3);
+    REQUIRE(map.contains(10));
+    REQUIRE(map.contains(20));
+    REQUIRE(map.contains(30));
+    map.remove(10);
+    REQUIRE(map.size() == 2);
+    REQUIRE(!map.contains(10));
+    REQUIRE(map.contains(20));
+    REQUIRE(map.contains(30));
+    map.remove(10);
+    REQUIRE(map.size() == 2);
+    REQUIRE(map.contains(20));
+    REQUIRE(map.contains(30));
+}
+
+TEST_CASE("SmallMap::range_for", "[test]")
+{
+    SmallMap<uint32_t, uint32_t, 5> map;
+    // Make sure this skips
+    for (auto &v : map)
+        v.second++;
+
+    map = init_test_small_map_u32<5>(3);
+
+    for (auto &v : map)
+        v.second++;
+
+    REQUIRE(map.contains(10));
+    REQUIRE(map.contains(20));
+    REQUIRE(map.contains(30));
+
+    SmallMap<uint32_t, uint32_t, 5> const &map_const = map;
+    uint32_t sum = 0;
+    for (auto const &v : map_const)
+        sum += v.second;
+    REQUIRE(sum == 66);
+}
+
+TEST_CASE("SmallMap::aligned", "[test]")
+{
+    CstdlibAllocator allocator;
+
+    SmallMap<AlignedObj, AlignedObj, 2> map;
+
+    map.insert_or_assign(make_pair(AlignedObj{10}, AlignedObj{11}));
+    map.insert_or_assign(make_pair(AlignedObj{20}, AlignedObj{21}));
+
+    REQUIRE(map.contains({10}));
+    REQUIRE(map.contains({20}));
+
+    uint32_t sum = 0;
+    for (auto const &v : map)
+        sum += v.second.value;
+    REQUIRE(sum == 32);
 }
