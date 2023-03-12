c8a30207ce9fecb7e2a598b1a6aeb87dbaeeba86
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-03-12 12:39:23 +0200

    Add Optional

---
+++ containers/optional.hpp
@@ -0,0 +1,145 @@
+#ifndef WHEELS_OPTIONAL_HPP
+#define WHEELS_OPTIONAL_HPP
+
+#include "container_utils.hpp"
+
+namespace wheels
+{
+
+template <typename T> class Optional
+{
+    // TODO: Constrain stored type ctor, dtor to noexcept?
+  public:
+    Optional() = default;
+    Optional(T const &value);
+    Optional(T &&value);
+    ~Optional();
+
+    Optional(Optional<T> const &other);
+    Optional(Optional<T> &&other);
+    Optional<T> &operator=(Optional<T> const &other);
+    Optional<T> &operator=(Optional<T> &&other);
+
+    bool has_value() const noexcept;
+    void reset();
+    template <typename... Args> void emplace(Args &&...args);
+
+    T &operator*() noexcept;
+    T const &operator*() const noexcept;
+    T *operator->() noexcept;
+    T const *operator->() const noexcept;
+
+  private:
+    alignas(T) uint8_t m_data[sizeof(T)];
+    bool m_has_value{false};
+};
+
+template <typename T> Optional<T>::Optional(T const &value)
+{
+    m_has_value = true;
+    new (m_data) T{value};
+}
+
+template <typename T> Optional<T>::Optional(T &&value)
+{
+    m_has_value = true;
+    new (m_data) T{WHEELS_MOV(value)};
+}
+
+template <typename T> Optional<T>::~Optional() { reset(); }
+
+template <typename T>
+Optional<T>::Optional(Optional<T> const &other)
+: m_has_value{other.m_has_value}
+{
+    if (m_has_value)
+        new (m_data) T{*(T *)other.m_data};
+}
+
+template <typename T>
+Optional<T>::Optional(Optional<T> &&other)
+: m_has_value{other.m_has_value}
+{
+    if (m_has_value)
+        new (m_data) T{WHEELS_MOV(*(T *)other.m_data)};
+    other.m_has_value = false;
+}
+
+template <typename T>
+Optional<T> &Optional<T>::operator=(Optional<T> const &other)
+{
+    if (this != &other)
+    {
+        reset();
+
+        if (other.m_has_value)
+            new (m_data) T{*(T *)other.m_data};
+        m_has_value = other.m_has_value;
+    }
+    return *this;
+}
+
+template <typename T> Optional<T> &Optional<T>::operator=(Optional<T> &&other)
+{
+    if (this != &other)
+    {
+        reset();
+
+        if (other.m_has_value)
+            new (m_data) T{WHEELS_MOV(*(T *)other.m_data)};
+        m_has_value = other.m_has_value;
+        other.m_has_value = false;
+    }
+    return *this;
+}
+
+template <typename T> bool Optional<T>::has_value() const noexcept
+{
+    // TODO:
+    // Does prefetching make a difference when data resides before this flag?
+    // Caller likely uses the data when we return true here
+    return m_has_value;
+}
+
+template <typename T> void Optional<T>::reset()
+{
+    if (m_has_value)
+    {
+        (*(T *)m_data).~T();
+        m_has_value = false;
+    }
+}
+
+template <typename T>
+template <typename... Args>
+void Optional<T>::emplace(Args &&...args)
+{
+    reset();
+
+    new (m_data) T{WHEELS_MOV(args)...};
+    m_has_value = true;
+}
+
+template <typename T> T &Optional<T>::operator*() noexcept
+{
+    return *(T *)m_data;
+}
+
+template <typename T> T const &Optional<T>::operator*() const noexcept
+{
+    return *(T *)m_data;
+}
+
+template <typename T> T *Optional<T>::operator->() noexcept
+{
+    return (T *)m_data;
+}
+
+template <typename T> T const *Optional<T>::operator->() const noexcept
+{
+    return (T const *)m_data;
+}
+
+} // namespace wheels
+
+#endif // WHEELS_OPTIONAL_HPP
--- containers/test.cpp
+++ containers/test.cpp
@@ -4,6 +4,7 @@
 #include "array.hpp"
 #include "hash.hpp"
 #include "hash_set.hpp"
+#include "optional.hpp"
 #include "pair.hpp"
 #include "small_map.hpp"
 #include "small_set.hpp"
@@ -1433,3 +1434,198 @@ HASH_TEST_IMPLEMENTATION(double);
 #undef HASH_TEST_IMPLEMENTATIOn
 #undef STR
 #undef STR2
+
+TEST_CASE("Optional::ctors")
+{
+    {
+        Optional<uint32_t> opt;
+        REQUIRE(!opt.has_value());
+    }
+
+    {
+        init_dtor_counters();
+        DtorObj const val{2};
+        Optional<DtorObj> opt{val};
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_copy_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt.has_value());
+        REQUIRE((*opt).data == 2);
+        REQUIRE(opt->data == 2);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 2);
+
+    {
+        init_dtor_counters();
+        DtorObj val{2};
+        Optional<DtorObj> opt{WHEELS_MOV(val)};
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_move_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt.has_value());
+        REQUIRE((*opt).data == 2);
+        REQUIRE(opt->data == 2);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 1);
+
+    {
+        init_dtor_counters();
+        Optional<DtorObj> opt{DtorObj{2}};
+        Optional<DtorObj> opt2{opt};
+        REQUIRE(DtorObj::s_ctor_counter == 3);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_move_ctor_counter == 1);
+        REQUIRE(DtorObj::s_copy_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt2.has_value());
+        REQUIRE((*opt2).data == 2);
+        REQUIRE(opt2->data == 2);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 2);
+
+    {
+        init_dtor_counters();
+        Optional<DtorObj> opt{DtorObj{2}};
+        Optional<DtorObj> opt2{WHEELS_MOV(opt)};
+        REQUIRE(DtorObj::s_ctor_counter == 3);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_move_ctor_counter == 2);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt2.has_value());
+        REQUIRE((*opt2).data == 2);
+        REQUIRE(opt2->data == 2);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 1);
+}
+
+TEST_CASE("Optional::assignments")
+{
+    {
+        init_dtor_counters();
+        Optional<DtorObj> opt;
+        Optional<DtorObj> opt2;
+        REQUIRE(DtorObj::s_ctor_counter == 0);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(!opt.has_value());
+        REQUIRE(!opt2.has_value());
+
+        opt2 = opt;
+        REQUIRE(DtorObj::s_ctor_counter == 0);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(!opt.has_value());
+        REQUIRE(!opt2.has_value());
+
+        opt2 = WHEELS_MOV(opt);
+        REQUIRE(DtorObj::s_ctor_counter == 0);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(!opt2.has_value());
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 0);
+
+    {
+        init_dtor_counters();
+        Optional<DtorObj> const opt{DtorObj{2}};
+        Optional<DtorObj> opt2;
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_move_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt.has_value());
+        REQUIRE((*opt).data == 2);
+        REQUIRE(opt->data == 2);
+        REQUIRE(!opt2.has_value());
+
+        opt2 = opt;
+
+        REQUIRE(DtorObj::s_ctor_counter == 3);
+        REQUIRE(DtorObj::s_copy_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt.has_value());
+        REQUIRE((*opt).data == 2);
+        REQUIRE(opt->data == 2);
+        REQUIRE(opt2.has_value());
+        REQUIRE(opt2->data == 2);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 2);
+
+    {
+        init_dtor_counters();
+        Optional<DtorObj> opt{DtorObj{2}};
+        Optional<DtorObj> opt2;
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_move_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt.has_value());
+        REQUIRE(opt->data == 2);
+        REQUIRE(!opt2.has_value());
+
+        opt2 = WHEELS_MOV(opt);
+
+        REQUIRE(DtorObj::s_ctor_counter == 3);
+        REQUIRE(DtorObj::s_move_ctor_counter == 2);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt2.has_value());
+        REQUIRE(opt2->data == 2);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 1);
+}
+
+TEST_CASE("Optional::aligned")
+{
+    AlignedObj const value{2};
+    Optional<AlignedObj> map{value};
+    REQUIRE(map.has_value());
+    REQUIRE(*map == value);
+    REQUIRE(map->value == value.value);
+}
+
+TEST_CASE("Optional::emplace")
+{
+    init_dtor_counters();
+    Optional<DtorObj> opt;
+    opt.emplace(2u);
+    REQUIRE(DtorObj::s_ctor_counter == 1);
+    REQUIRE(DtorObj::s_value_ctor_counter == 1);
+    REQUIRE(DtorObj::s_assign_counter == 0);
+    REQUIRE(DtorObj::s_dtor_counter == 0);
+    REQUIRE(opt.has_value());
+    REQUIRE(opt->data == 2);
+}
+
+TEST_CASE("Optional::reset")
+{
+    {
+        init_dtor_counters();
+        Optional<DtorObj> opt{DtorObj{2}};
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_value_ctor_counter == 1);
+        REQUIRE(DtorObj::s_move_ctor_counter == 1);
+        REQUIRE(DtorObj::s_assign_counter == 0);
+        REQUIRE(DtorObj::s_dtor_counter == 0);
+        REQUIRE(opt.has_value());
+
+        opt.reset();
+        REQUIRE(!opt.has_value());
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_dtor_counter == 1);
+
+        opt.reset();
+        REQUIRE(!opt.has_value());
+        REQUIRE(DtorObj::s_ctor_counter == 2);
+        REQUIRE(DtorObj::s_dtor_counter == 1);
+    }
+    REQUIRE(DtorObj::s_dtor_counter == 1);
+}
