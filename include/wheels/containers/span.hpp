4e5b11ae94b1560588beb88419444b1cc9da407a
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-04-13 20:12:10 +0300

    span: Add comparisons

--- include/wheels/containers/span.hpp
+++ include/wheels/containers/span.hpp
@@ -2,6 +2,8 @@
 #ifndef WHEELS_CONTAINERS_SPAN_HPP
 #define WHEELS_CONTAINERS_SPAN_HPP
 
+#include "concepts.hpp"
+
 #include <cstddef>
 
 namespace wheels
@@ -74,6 +76,76 @@ template <typename T> size_t Span<T>::size() const { return m_size; }
 template <typename T> Span<T>::operator Span<T const>() const
 {
     return Span<T const>{m_data, m_size};
+}
+
+// Compires the entire spans, no special handling for e.g. trailing nulls in
+// Span<char>. Agnostic to inner const.
+template <typename T, typename V>
+    requires SameAs<T, V> bool
+operator==(Span<T> lhs, Span<V> rhs)
+{
+    if (lhs.size() != rhs.size())
+        return false;
+
+    if constexpr (std::is_arithmetic_v<T> || std::is_pointer_v<T>)
+        return memcmp(lhs.data(), rhs.data(), lhs.size() * sizeof(T)) == 0;
+    else
+    {
+        size_t const size = lhs.size();
+        for (size_t i = 0; i < size; ++i)
+        {
+            if (lhs.data()[i] != rhs.data()[i])
+                return false;
+        }
+    }
+
+    return true;
+}
+
+// Compires the entire spans, no special handling for e.g. trailing nulls in
+// Span<char>. Agnostic to inner const.
+template <typename T, typename V>
+    requires SameAs<T, V> bool
+operator!=(Span<T> lhs, Span<T> rhs)
+{
+    return !(lhs == rhs);
+}
+
+// Returns true if the spans are equal as c-strings, treating data()[size()] as
+// \0. Compares the spans assuming they are views into c-strings exluding the
+// final null. This includes the assumption that the character at data()[size()]
+// is allocated and may or may not be \0, depending on the span being a full
+// view into the string or not.
+template <typename T, typename V>
+    requires(SameAs<T, char> && SameAs<V, char>) bool
+compare_str(Span<T> lhs, Span<V> rhs)
+{
+    size_t first_len = 0;
+    size_t second_len = 0;
+    char const *first_ptr = nullptr;
+    char const *second_ptr = nullptr;
+    if (lhs.size() < rhs.size())
+    {
+        first_len = lhs.size();
+        second_len = rhs.size();
+        first_ptr = lhs.data();
+        second_ptr = rhs.data();
+    }
+    else
+    {
+        first_len = rhs.size();
+        second_len = lhs.size();
+        first_ptr = rhs.data();
+        second_ptr = lhs.data();
+    }
+
+    // Compare by the shorter span
+    if (strncmp(first_ptr, second_ptr, first_len) != 0)
+        return false;
+
+    // Make sure the spans are the same size of the longer span ends in null
+    // after the common part
+    return first_len == second_len || second_ptr[first_len] == '\0';
 }
 
 } // namespace wheels
--- tests/containers/span.cpp
+++ tests/containers/span.cpp
@@ -5,6 +5,8 @@
 #include <wheels/containers/span.hpp>
 #include <wheels/containers/static_array.hpp>
 
+#include "common.hpp"
+
 using namespace wheels;
 
 TEST_CASE("Span::create")
@@ -72,4 +74,99 @@ TEST_CASE("Span::const_conversion")
     {
         Span<uint32_t const> const_span{span};
     }
+}
+
+TEST_CASE("Span::comparisons")
+{
+    {
+        uint32_t array[] = {0, 1, 2, 3};
+        uint32_t const array2[] = {0, 1, 3};
+
+        // No inner const to make sure that works
+        Span<uint32_t> const span_partial{
+            array, sizeof(array) / sizeof(uint32_t) - 1};
+        Span<uint32_t const> const span_full{
+            array, sizeof(array) / sizeof(uint32_t)};
+        Span<uint32_t const> const span_full2{
+            array2, sizeof(array2) / sizeof(uint32_t)};
+        Span<uint32_t const> const span_empty{array, 0};
+
+        REQUIRE(span_partial == span_partial);
+        REQUIRE(!(span_partial == span_full));
+        REQUIRE(!(span_partial == span_full2));
+        REQUIRE(!(span_full == span_partial));
+        REQUIRE(!(span_empty == span_partial));
+        REQUIRE(!(span_empty == span_partial));
+
+        REQUIRE(span_full != span_partial);
+        REQUIRE(span_partial != span_full);
+        REQUIRE(span_partial != span_full2);
+        REQUIRE(span_empty != span_partial);
+        REQUIRE(span_partial != span_empty);
+        REQUIRE(!(span_partial != span_partial));
+    }
+
+    {
+        DtorObj const array[] = {{0}, {1}, {2}, {3}};
+        DtorObj const array2[] = {{0}, {1}, {3}};
+
+        Span<DtorObj const> const span_partial{
+            array, sizeof(array) / sizeof(DtorObj) - 1};
+        Span<DtorObj const> const span_full{
+            array, sizeof(array) / sizeof(DtorObj)};
+        Span<DtorObj const> const span_full2{
+            array2, sizeof(array2) / sizeof(DtorObj)};
+        Span<DtorObj const> const span_empty{array, 0};
+
+        REQUIRE(span_partial == span_partial);
+        REQUIRE(!(span_partial == span_full));
+        REQUIRE(!(span_partial == span_full2));
+        REQUIRE(!(span_full == span_partial));
+        REQUIRE(!(span_empty == span_partial));
+        REQUIRE(!(span_empty == span_partial));
+
+        REQUIRE(span_full != span_partial);
+        REQUIRE(span_partial != span_full);
+        REQUIRE(span_partial != span_full2);
+        REQUIRE(span_empty != span_partial);
+        REQUIRE(span_partial != span_empty);
+        REQUIRE(!(span_partial != span_partial));
+    }
+}
+
+TEST_CASE("Span::compare_str")
+{
+    // Behavior should be equal to c-string comparisons with the added flavor
+    // that a truncating view is treated like the next character was \0.
+
+    char cempty[] = "";
+    char const ctest[] = "test";
+    char const ctester[] = "tester";
+    char const ctett[] = "tett";
+    char const ctestnull[] = "test\0\0\0";
+
+    Span<char> empty{cempty, 0};
+    Span<char const> empty2{cempty, 0};
+    Span<char const> test{ctest, sizeof(ctest) - 1};
+    Span<char const> test2{ctester, sizeof(ctester) - 3};
+    Span<char const> tett{ctett, sizeof(ctett) - 1};
+    Span<char const> tester{ctester, sizeof(ctester) - 1};
+    Span<char const> testnull{ctestnull, sizeof(ctestnull) - 1};
+
+    REQUIRE(compare_str(empty, empty));
+    REQUIRE(compare_str(empty, empty2));
+    REQUIRE(compare_str(empty2, empty));
+    REQUIRE(!compare_str(empty, test));
+    REQUIRE(!compare_str(test, empty));
+
+    REQUIRE(compare_str(test, test));
+    REQUIRE(compare_str(test, test2));
+    REQUIRE(compare_str(test2, test2));
+    REQUIRE(compare_str(test2, test2));
+    REQUIRE(!compare_str(test, tett));
+    REQUIRE(!compare_str(tett, test));
+    REQUIRE(!compare_str(test, tester));
+    REQUIRE(!compare_str(tester, test));
+    REQUIRE(compare_str(test, testnull));
+    REQUIRE(compare_str(testnull, test));
 }
