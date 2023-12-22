c99a8a2303599f4814abd2cb000ac7b2944d6c53
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-12-22 16:31:32 +0200

    Make Pair constexpr

--- include/wheels/containers/pair.hpp
+++ include/wheels/containers/pair.hpp
@@ -13,24 +13,24 @@ template <typename T, typename V> struct Pair
     T first;
     V second;
 
-    Pair()
+    constexpr Pair()
         requires(std::default_initializable<T> && std::default_initializable<V>)
     = default;
 
     // Let's be pedantic and disallow implicit conversions
     template <typename U, typename W>
         requires(SameAs<U, T> && SameAs<W, V>)
-    Pair(U &&first, W &&second);
+    constexpr Pair(U &&first, W &&second);
     template <typename U, typename W>
         requires(SameAs<U, T> && SameAs<W, V>)
-    Pair(Pair<U, W> &&other);
+    constexpr Pair(Pair<U, W> &&other);
     template <typename U, typename W>
         requires(SameAs<U, T> && SameAs<W, V>)
-    Pair<T, V> &operator=(Pair<U, W> &&other);
+    constexpr Pair<T, V> &operator=(Pair<U, W> &&other);
 };
 
 template <typename T, typename V>
-[[nodiscard]] Pair<T, V> make_pair(T &&first, V &&second)
+[[nodiscard]] constexpr Pair<T, V> make_pair(T &&first, V &&second)
 {
     return Pair<T, V>{WHEELS_FWD(first), WHEELS_FWD(second)};
 }
@@ -40,21 +40,21 @@ template <typename T, typename V> Pair(T &&first, V &&second) -> Pair<T, V>;
 template <typename T, typename V>
 template <typename U, typename W>
     requires(SameAs<U, T> && SameAs<W, V>)
-Pair<T, V>::Pair(U &&first, W &&second)
+constexpr Pair<T, V>::Pair(U &&first, W &&second)
 : first{WHEELS_FWD(first)}
 , second{WHEELS_FWD(second)} {};
 
 template <typename T, typename V>
 template <typename U, typename W>
     requires(SameAs<U, T> && SameAs<W, V>)
-Pair<T, V>::Pair(Pair<U, W> &&other)
+constexpr Pair<T, V>::Pair(Pair<U, W> &&other)
 : first{WHEELS_MOV(other.first)}
 , second{WHEELS_MOV(other.second)} {};
 
 template <typename T, typename V>
 template <typename U, typename W>
     requires(SameAs<U, T> && SameAs<W, V>)
-Pair<T, V> &Pair<T, V>::operator=(Pair<U, W> &&other)
+constexpr Pair<T, V> &Pair<T, V>::operator=(Pair<U, W> &&other)
 {
     if (this != &other)
     {
@@ -65,13 +65,15 @@ Pair<T, V> &Pair<T, V>::operator=(Pair<U, W> &&other)
 }
 
 template <typename T, typename V>
-[[nodiscard]] bool operator==(Pair<T, V> const &lhs, Pair<T, V> const &rhs)
+[[nodiscard]] constexpr bool operator==(
+    Pair<T, V> const &lhs, Pair<T, V> const &rhs)
 {
     return lhs.first == rhs.first && lhs.second == rhs.second;
 }
 
 template <typename T, typename V>
-[[nodiscard]] bool operator!=(Pair<T, V> const &lhs, Pair<T, V> const &rhs)
+[[nodiscard]] constexpr bool operator!=(
+    Pair<T, V> const &lhs, Pair<T, V> const &rhs)
 {
     return lhs.first != rhs.first || lhs.second != rhs.second;
 }
--- tests/containers/pair.cpp
+++ tests/containers/pair.cpp
@@ -6,6 +6,33 @@
 
 using namespace wheels;
 
+namespace
+{
+
+constexpr uint32_t test_constexpr()
+{
+    uint32_t zero = 0;
+    uint32_t one = 1;
+    uint16_t two = 2;
+    uint16_t three = 3;
+    Pair<uint32_t, uint16_t> p0{zero, two};
+    // This should still typecheck rvalue interface?
+    // NOLINTNEXTLINE(performance-move-const-arg)
+    Pair<uint32_t, uint16_t> p1{0u, std::move(two)};
+    // This should still typecheck rvalue interface?
+    // NOLINTNEXTLINE(performance-move-const-arg)
+    Pair<uint32_t, uint16_t> p2{std::move(zero), three};
+    // This should still typecheck rvalue interface?
+    // NOLINTNEXTLINE(performance-move-const-arg)
+    Pair<uint32_t, uint16_t> p3{std::move(one), std::move(three)};
+    Pair<uint32_t, uint16_t> p4 = make_pair((uint32_t)3, (uint16_t)1);
+
+    return (uint32_t)(p0 == p1) + (uint32_t)(p0 != p2) + (uint32_t)(p0 != p3) +
+           (uint32_t)(p0 != p4);
+}
+
+} // namespace
+
 TEST_CASE("Pair")
 {
     uint32_t deadcafe = 0xDEADCAFE;
@@ -38,4 +65,10 @@ TEST_CASE("Pair")
     REQUIRE(p0 == p1);
     REQUIRE(p0 != p2);
     REQUIRE(p0 != p4);
+}
+
+TEST_CASE("constexpr Pair")
+{
+    constexpr uint32_t result = test_constexpr();
+    REQUIRE(result == 4);
 }
